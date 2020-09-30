6e733cec2761131a32837215701ebb0ae078a139
Fendly Belhomme <f.belhomme.jouvet@gmail.com>
2020-09-30 15:42:58 +0200

    new page...

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/TestController', function () {
+    return view('testPage');
+});
